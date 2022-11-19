5460c76cc29dd411afa43b1df759dbeef907dd45
Anjith Mathew <anjithmathew777@gmail.com>
2022-11-19 09:02:07 +0530

    modified

--- 3.Games/1.Tic Tac Toe/basic tic tac toe.py
+++ 3.Games/1.Tic Tac Toe/basic tic tac toe.py
@@ -1,6 +1,8 @@
 tic = [[1,1,1],[1,1,1],[1,1,1]]
-for i in tic:
-    print(i)
+print("   0  1  2")
+
+for count,row in enumerate(tic):
+    print(count,row)
 
 user_input =input("Enter where do you want to input")
 print(user_input)
